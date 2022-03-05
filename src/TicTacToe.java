2ec7bb5c0e071efab3e8e623742084375a3a18b8
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2022-03-05 17:08:12 +0100

    La till solo mode

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -6,13 +6,14 @@ public class TicTacToe {
         String p1X;
         String p2O;
         char[] spelplan = new char[9];
+        int ai = JOptionPane.showConfirmDialog(null, "Solo mode mot ''ai'' eller 1v1 mot person i rummet? (klicka på ja för solo mode)", "Solo mode?", JOptionPane.YES_NO_OPTION);
 
         for (int j = 0; j < 10; j++) {
             Arrays.fill(spelplan, ' ');
 
             visaSpelplan(spelplan);
             for (int i = 0; i < 69; i++) {
-                if (i == 0 || i == 2 || i == 4 || i == 6 || i == 8 || i == 10) {
+                if (i == 0 || i == 2 || i == 4 || i == 6 || i == 8 || i == 10 || i == 12 || i == 14) {
                     p1X = JOptionPane.showInputDialog("Spelare X: " + "\nVilken ruta?(1-9)");
                     int p1Xi = Integer.parseInt(p1X);
                     p1Xi = p1Xi - 1;
@@ -20,12 +21,23 @@ public class TicTacToe {
                         spelplan[p1Xi] = 'X';
                     }
                 } else {
-                    if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9) {
-                        p2O = JOptionPane.showInputDialog("Spelare O: " + "\nVilken ruta?(1-9)");
-                        int p2Oi = Integer.parseInt(p2O);
-                        p2Oi = p2Oi - 1;
-                        if (spelplan[p2Oi] == ' ') {
-                            spelplan[p2Oi] = 'O';
+                    if (i == 1 || i == 3 || i == 5 || i == 7 || i == 9 || i == 11 || i == 13) {
+                        if (ai == 1) {
+                            p2O = JOptionPane.showInputDialog("Spelare O: " + "\nVilken ruta?(1-9)");
+                            int p2Oi = Integer.parseInt(p2O);
+                            p2Oi = p2Oi - 1;
+                            if (spelplan[p2Oi] == ' ') {
+                                spelplan[p2Oi] = 'O';
+                            }
+                        }
+                        else {
+                            while (true) {
+                                int random = (int) (Math.random() * 9);
+                                if (spelplan[random] == ' ') {
+                                    spelplan[random] = 'O';
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
